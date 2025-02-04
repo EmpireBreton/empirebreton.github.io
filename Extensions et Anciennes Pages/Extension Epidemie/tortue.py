bddf37136962336c1e8d8c39b434113d34d56d33
Gaël <89847466+gaelBZH@users.noreply.github.com>
2025-02-04 19:56:14 +0100

    Delete Extensions et Anciennes Pages directory

--- Extensions et Anciennes Pages/Extension Epidemie/tortue.py
+++
@@ -1,9 +0,0 @@
-from turtle import *
-colore=["red", "green", "blue"]*10
-speed(100)
-for j in range(4):
-    for i in range(10,100,10):
-        color(colore[(i-10)//10])
-        circle(i)
-    right(90)
-done()
